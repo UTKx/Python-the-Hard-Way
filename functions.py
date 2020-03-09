5edc686b6044ce6121c2d77f780a669d64124376
Utkarsh <utkarshkumark@gmail.com>
2020-03-09 21:00:51 +0530

    6th commit

--- functions.py
+++ functions.py
@@ -3,3 +3,48 @@
 # > They take arguments the way your scripts take argv.
 # > Using 1 and 2, they let you make your own “mini-scripts” or “tiny commands.”
 
+# this one is like your scripts with argv
+def print_two(*args): # *args (asterisk args), which is a lot like your argv parameter but for functions.
+    arg1, arg2 = args # * in *args tells python to take all the arguments to the function and then put them in args as a list. It’s like argv that you’ve been using but for functions
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# this just takes one argument
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# this one takes no arguments
+def print_none():
+    print("I got nothin'.")
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
+print()
+
+# Example
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+    print(type(cheese_count))
+    print(type(boxes_of_crackers))
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
+print("OR, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print("We can even do math inside too:")
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
