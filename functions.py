ddafdcce881b3dce5941d339408d9187e749a76c
Utkarsh <utkarshkumark@gmail.com>
2020-03-10 18:14:18 +0530

    6th commit

---
+++ functions.py
@@ -0,0 +1,5 @@
+# Functions do three things:
+# > They name pieces of code the way variables name strings and numbers.
+# > They take arguments the way your scripts take argv.
+# > Using 1 and 2, they let you make your own “mini-scripts” or “tiny commands.”
+
