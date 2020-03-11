6e2c2ade14f2621c083c9120ad4914bf3e74e836
Utkarsh <utkarshkumark@gmail.com>
2020-03-11 21:17:46 +0530

    9th commit

---
+++ loops.py
@@ -0,0 +1,52 @@
+# Loops and list
+
+# for loop
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:
+    print(f"This is count {number}")
+
+# same as above
+for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+# also we can go through mixed lists too
+# notice we have to use {} since we don't know what's in it
+for i in change:
+    print(f"I got {i}")
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+    print(f"Adding {i} to the list.")
+    # append is a function that lists understand
+    elements.append(i)
+
+# now we can print them out too
+for i in elements:
+    print(f"Element was: {i}")
+print()
+
+# while loop
+# do is simply do a test like an if-statement, but instead of running
+# the code block once, they jump back to the “top” where the while is and repeat. A while-loop runs
+# until the expression is False.
+i = 0
+numbers = []
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
