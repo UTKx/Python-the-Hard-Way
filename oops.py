e2246690f44d1c07b3d5fbedcb3d14f2b98274c0
Utkarsh <utkarshkumark@gmail.com>
2020-03-13 23:34:56 +0530

    Final commit

--- oops.py
+++ oops.py
@@ -41,3 +41,99 @@ bulls_on_parade = Song(["They rally around tha family",
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
+
+# Inheritance : Inheritance is used to indicate that one class will get most or all of its features from a parent class.
+# There are three ways that the parent and child classes
+# can interact:
+# 1. Actions on the child imply an action on the parent.
+# 2. Actions on the child override the action on the parent.
+# 3. Actions on the child alter the action on the parent.
+
+# Implicit inheritance
+class Parent():
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()  # Notice how even though I’m calling son.implicit() on line 13 and 
+                # even though Child does not have an implicit function defined, it 
+                # still works, and it calls the one defined in Parent. This shows you that
+                # if you put functions in a base class (i.e., Parent), then all subclasses 
+                # (i.e., Child) will automatically get those features. 
+                # Very handy for repetitive code you need in many classes.
+
+# Override Explicitly
+class Parent():
+
+    def override(self):
+        print("PARENT override()")
+
+class Child(Parent):
+    def override(self):
+        print("CHILD override()")
+
+dad = Parent()
+son = Child()
+
+dad.override() #  when line 14 runs, it runs the Parent.override function because that variable (dad) is a Parent.
+son.override() #  But when line 15 runs, it prints out the Child.override messages because son is an
+               # instance of Child and Child overrides that function by defining its own version.
+
+# Alter
+class Parent():
+    def altered(self):
+        print("PARENT altered()")
+
+class Child(Parent):
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered() # 
+        print("CHILD, AFTER PARENT altered()")
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
+
+# TODO: super() && Method Resolution Order(MRO)
+
+# Composition
+class Other():
+
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child():
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE OTHER altered()")
+        self.other.altered()
+        print("CHILD, AFTER OTHER altered()")
+
+son = Child()
+son.implicit()
+son.override()
+son.altered()
